bb52cc312dbd72ff185f6731862339c873c630db
Moritz <ninj24o@gmail.com>
2023-03-03 16:25:57 +0100

    first model and controller test

---
+++ matlab/controller_design.m
@@ -0,0 +1,10 @@
+load envs\linear_model_v001.mat;
+A_num = double(A_sub);
+b_num = double(b_sub);
+c = eye(4);
+d = [0; 0; 0; 0];
+init_cond = [0, 5, .3, .3];
+
+Q = [3 0 0 0; 0 5 0 0; 0 0 0 0; 0 0 0 0]
+R = 0.0013
+[F, P, E] = lqr(A_num, b_num, Q, R)
