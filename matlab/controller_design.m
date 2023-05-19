77399bf3fce65761c9eaa22135024b388c169ff3
Moritz <ninj24o@gmail.com>
2023-05-19 21:26:40 +0200

    Balance Controller

--- matlab/controller_design.m
+++ matlab/controller_design.m
@@ -3,9 +3,18 @@ A_num = double(A_sub);
 b_num = double(b_sub);
 c = eye(4);
 d = [0; 0; 0; 0];
-init_cond = [0, 5, .3, .3];
+init_cond = [0.4, 1, 0.2, .3];
 
-Q = [3 0 0 0; 0 5 0 0; 0 0 0 0; 0 0 0 0]
-R = 0.0013
-Ts = 0.004
+Q = [3 0 0 0; 0 5 0 0; 0 0 0 0; 0 0 0 0];
+R = 0.0013;
+Ts = 0.004;
 [F, P, E] = lqrd(A_num, b_num, Q, R, Ts)
+
+A_hat = [A_num zeros(4,1); [1 0 0 0], 0];
+b_hat = [b_num; 0];
+c_hat = [1 0 0 0 0];
+
+Q_new = [3 0 0 0 0; 0 5 0 0 0; 0 0 0 0 0; 0 0 0 0 0; 0 0 0 0 0.00001];
+R_new = 0.0013;
+
+[F_new, P_new, E_new] = lqrd(A_hat, b_hat, Q_new, R_new, Ts)
